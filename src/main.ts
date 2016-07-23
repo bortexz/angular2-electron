bc283ec379c2b7acf3293c3b8235a5a4b23af99c
Israel Gilyadov <israelg99@gmail.com>
2016-07-23 06:41:16 -0500

    Update dependencies; update code; README.md update (#1)
    * Update dependencies; update code
    
    * Add TODO to README.md
    
    Add TODO `Separate dev, test and production webpack configurations` to README.md

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HTTP_PROVIDERS } from '@angular/http';
@@ -11,8 +10,6 @@ const ENV_PROVIDERS = [];
 // depending on the env mode, enable prod mode or add debugging modules
 if (process.env.ENV === 'build') {
   enableProdMode();
-} else {
-  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
 }
 
 bootstrap(AppComponent, [
