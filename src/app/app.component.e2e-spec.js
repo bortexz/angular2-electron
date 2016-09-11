23dec268b6af3b1ef20b32c15c756d204bd00a97
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2016-09-11 17:52:09 +0100

    Added title on app.html

--- src/app/app.component.e2e-spec.js
+++ src/app/app.component.e2e-spec.js
@@ -1,22 +1,21 @@
 describe('App', function () {
   it('should have a title', function () {
-    expect(browser.getTitle()).toEqual("Angular 2 App | ng2-webpack");
-  });
+    expect(browser.getTitle()).toEqual('Angular 2 App | ng2-webpack')
+  })
 
   it('should have <header>', function () {
-    expect(element(by.css('my-app header')).isPresent()).toEqual(true);
-  });
+    expect(element(by.css('my-app header')).isPresent()).toEqual(true)
+  })
 
   it('should have <main>', function () {
-    expect(element(by.css('my-app main')).isPresent()).toEqual(true);
-  });
+    expect(element(by.css('my-app main')).isPresent()).toEqual(true)
+  })
 
   it('should have a main title', function () {
-    expect(element(by.css('main .title')).getText()).toEqual('Hello from Angular 2!');
-  });
+    expect(element(by.css('main .title')).getText()).toEqual('Hello from Angular 2!')
+  })
 
   it('should have <footer>', function () {
-    expect(element(by.css('my-app footer')).getText()).toEqual("Webpack Angular 2 Starter");
-  });
-
-});
+    expect(element(by.css('my-app footer')).getText()).toEqual('Webpack Angular 2 Starter')
+  })
+})
